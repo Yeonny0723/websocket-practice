b47aae56d3f9a47f6e93dcb3d83832fb5a7f13b2
Juyeon Kim <kkjuyeon@gmail.com>
2024-07-03 20:51:33 +0900

    #1 feat: create websocket server #1 #1 - create websocket server process both http & ws

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -6,6 +8,12 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000);
+const handleListen = () => console.log(`Listening on ws://localhost:3000`);
+
+// http & ws 프로토콜 모두 이해하는 서버 생성
+const server = http.createServer(app);
+const websocketServer = new WebSocket.Server({ server });
+
+server.listen(3000, handleListen);
