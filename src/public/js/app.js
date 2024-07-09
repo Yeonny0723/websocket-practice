46b9690fb467b1f389a796cdd5f2534e7779227c
Juyeon Kim <kkjuyeon@gmail.com>
2024-07-09 21:37:30 +0900

    feat: 다중 소캣 연결
    - 다중 소캣 연결
    - 메시지 전달 폼 생성

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,10 +1,24 @@
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
 const socket = new WebSocket(`ws://${window.location.host}`);
 
 socket.addEventListener("open", () => console.log("Connected to Server!"));
 setTimeout(() => {
   socket.send("Hello from Browser!");
 }, 10000);
-socket.addEventListener("message", (message) => console.log(message));
+socket.addEventListener("message", async (event) => {
+  console.log(`New Message: ${await event.data.text()}`);
+  const input = messageForm.querySelector("input");
+  input.value = "";
+});
 socket.addEventListener("close", () =>
   console.log("Disconnected from server!")
 );
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(input.value);
+}
+
+messageForm.addEventListener("submit", handleSubmit);
