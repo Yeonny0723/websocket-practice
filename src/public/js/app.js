c6262273fbb82c409a28c4d7d3cfd9f922364376
Juyeon Kim <kkjuyeon@gmail.com>
2024-08-12 22:06:09 +0900

    feat: 카메라 device 변경 select
    - 최초 렌더링에 선택된 device가 없을 땐 셀카 모드, 있을 땐 해당 device id를 갖는 카메라로 스위치

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -13,10 +13,15 @@ async function getCamerasAvailable() {
   try {
     const devices = await navigator.mediaDevices.enumerateDevices(); // 가능한 모든 장치
     const cameras = devices.filter((d) => d.kind === "videoinput");
+    const currentCamera = myStream.getVideoTracks()[0]; // 현재 비디오 device
     cameras.forEach((camera) => {
       const option = document.createElement("option");
       option.value = camera.deviceId;
       option.innerText = camera.label; // 기기명 추출
+      if (currentCamera.label === camera.label) {
+        // 선택된 기기 checked
+        option.selected = true;
+      }
       cameraSelects.appendChild(option);
     });
   } catch (e) {
@@ -24,14 +29,27 @@ async function getCamerasAvailable() {
   }
 }
 
-async function getVideoMedia() {
+async function getVideoMedia(deviceId) {
+  // 선택된 장치 ID
+  const initialConstraints = {
+    // 최초 렌더링 (devicdId가 없을 때) 셀카 모드
+    audio: true,
+    video: { facingMode: "user" },
+  };
+  const cameraConstraints = {
+    // 카메라 변경 시 해당 device로 switch
+    audio: true,
+    video: { deviceId: { exact: deviceId } },
+  };
   try {
-    myStream = await navigator.mediaDevices.getUserMedia({
-      audio: true,
-      video: true,
-    });
+    myStream = await navigator.mediaDevices.getUserMedia(
+      deviceId ? cameraConstraints : initialConstraints
+    );
     myVideoStream.srcObject = myStream; // video stream 연결
     await getCamerasAvailable();
+    if (!deviceId) {
+      await getCamerasAvailable();
+    }
   } catch (e) {
     console.log(e);
   }
@@ -63,7 +81,12 @@ function handleCameraCLick() {
   }
 }
 
+async function handleCameraChange() {
+  await getVideoMedia(cameraSelects.value);
+}
+
 muteBtn.addEventListener("click", handleMuteClick);
 cameraSwitchBtn.addEventListener("click", handleCameraCLick);
+cameraSelects.addEventListener("input", handleCameraChange);
 
 getVideoMedia();
