67e68c470680483e7b6bd1d610b7e66cf4161272
Juyeon Kim <kkjuyeon@gmail.com>
2024-08-12 21:44:56 +0900

    feat: 비디오 오디오 트랙 속성 변경 (mute, off)

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -3,24 +3,44 @@ const videoContainer = document.getElementById("videoContainer");
 const myVideoStream = document.getElementById("myVideo");
 const muteBtn = document.getElementById("muteBtn");
 const cameraSwitchBtn = document.getElementById("cameraSwitchBtn");
+const cameraSelects = document.getElementById("cameraSelects");
 
-let videoStream;
+let myStream;
 let cameraOn = false;
 let muteOn = false;
 
+async function getCamerasAvailable() {
+  try {
+    const devices = await navigator.mediaDevices.enumerateDevices(); // 가능한 모든 장치
+    const cameras = devices.filter((d) => d.kind === "videoinput");
+    cameras.forEach((camera) => {
+      const option = document.createElement("option");
+      option.value = camera.deviceId;
+      option.innerText = camera.label; // 기기명 추출
+      cameraSelects.appendChild(option);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
 async function getVideoMedia() {
   try {
-    videoStream = await navigator.mediaDevices.getUserMedia({
+    myStream = await navigator.mediaDevices.getUserMedia({
       audio: true,
       video: true,
     });
-    myVideoStream.srcObject = videoStream; // video stream 연결
+    myVideoStream.srcObject = myStream; // video stream 연결
+    await getCamerasAvailable();
   } catch (e) {
     console.log(e);
   }
 }
 
 function handleMuteClick() {
+  myStream
+    .getAudioTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
   if (muteOn) {
     muteBtn.innerText = "Mute";
     muteOn = false;
@@ -31,6 +51,9 @@ function handleMuteClick() {
 }
 
 function handleCameraCLick() {
+  myStream
+    .getVideoTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
   if (cameraOn) {
     cameraSwitchBtn.innerText = "Off";
     cameraOn = false;
